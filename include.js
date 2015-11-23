8b01bf9f696693a251d31c9bf66ac9da9821a02a
ADoyle <adoyle.h@gmail.com>
2015-11-23 21:23:06 +0800

    更新 include

--- include.js
+++ include.js
@@ -67,10 +67,11 @@ exports = module.exports = {
 };
 
 
-// 如果你将本文件放在 src/lib/require.js 里，相对路径就应该填 '../../'
+// 始终将 projectDir 设置成你的项目根目录的绝对路径。
 var projectDir = process.env.NODE_ROOT || Path.resolve(__dirname, '.');
 
-// 为了不额外创建一个文件，我将目录映射写在这个文件中
+// 为了不额外创建一个文件，我将目录映射写在这个文件中。
+// 默认会有一个 'proj'，指向 projectDir 的路径。
 init(projectDir, {
     map: {
         'assets': 'assets',
