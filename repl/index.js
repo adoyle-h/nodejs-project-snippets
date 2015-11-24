fd6dfb07186d78aed198236f236de06b1a43a930
ADoyle <adoyle.h@gmail.com>
2015-11-24 22:49:44 +0800

    移动 repl 文件到目录

--- repl/index.js
+++ repl/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var include = require('./include').include;
-var repl = include('lib/_repl');
+var include = require('../include').include;
+var repl = require('./repl');
 
 var contents = {
+    include: include,
     util: include('lib/util'),
     config: include('lib/config'),
     app: include('proj/app'),
     os: require('os'),
     CONSTS: include('src/CONSTS'),
-    include: include,
     logger: include('lib/log').create(module),
 };
 
