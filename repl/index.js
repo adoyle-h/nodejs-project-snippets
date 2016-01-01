5e4c4651f25a3ec7091692029c2a8bc49982aab9
ADoyle <adoyle.h@gmail.com>
2016-01-01 21:54:06 +0800

    refactor(repl): 移除 app，因为路径不一样了

--- repl/index.js
+++ repl/index.js
@@ -7,7 +7,6 @@ var contents = {
     include: include,
     util: include('lib/util'),
     config: include('lib/config'),
-    app: include('proj/app'),
     os: require('os'),
     CONSTS: include('src/CONSTS'),
     logger: include('lib/log').create(module),
